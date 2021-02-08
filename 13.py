ceb3e4e901a086497fdab4f0920aa8be734f3bd9
Ujjwal Sharma <ujjwal3sharma@gmail.com>
2021-02-08 18:33:53 +0000

    13 done

--- 13.py
+++ 13.py
@@ -1,6 +1,28 @@
+# Input a list of numbers and test if a number is equal to the sum of the cubes of its digits. Find the smallest and largest such number from the given list of numbers.
+
+
+def Armstrong(arr):
+  new_list =[]
+  for num in arr:
+    sum = 0
+    temp = num
+    while temp > 0:
+       digit = temp % 10
+       sum += digit ** 3
+       temp //= 10
+    if num == sum:
+       new_list.append(num)
+  new_list.sort()
+  smallest = new_list[0]
+  largest = new_list[-1]
+  print(f'smallest value is {smallest}')
+  print(f'largest value is {largest}')
+
 
 k = True
 while k == True:
+    arr = list(map(int, input('Enter a list of numbers: ').split(',')))
+    Armstrong(arr)
     option = input('Do you want to check more no.(y/n): ').lower()
     if option == 'y':
         continue
